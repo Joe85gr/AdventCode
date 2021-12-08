6fbf80a7ac97110d7599a76edfbb016d3accfa8b
joe85gr <joe85gr>
2021-12-08 08:39:12 +0000

    feat: Day 8 part 1 partial added

---
+++ Day8/FirstPart.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day8
+{
+    public static class FirstPart
+    {
+        public static double GetResult(IEnumerable<string> fileLines)
+        {
+
+            var test = fileLines
+                .Select(l => l.Split(" | "));
+            
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Day8/SecondPart.cs
@@ -0,0 +1,10 @@
+namespace Day8
+{
+    public class SecondPart
+    {
+        public static object GetResult(string[] fileLines)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
