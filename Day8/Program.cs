1a34413e51eb7d3e0bffa3a6d8bec6bb3f708b82
joe85gr <joe85gr>
2021-12-08 20:59:15 +0000

    feat: Day 8 uncommented lines

--- Day8/Program.cs
+++ Day8/Program.cs
@@ -12,11 +12,11 @@ namespace Day8
             var fileLines = File.ReadAllLines(FilePath);
             var watch = new System.Diagnostics.Stopwatch();
 
-            // watch.Start();
-            // var firstPartResult = FirstPart.GetResult(fileLines);
-            // watch.Stop();
-            // Console.WriteLine($"Day 8 - First Part result: {firstPartResult}");
-            // Console.WriteLine($"Elapsed time: {watch.ElapsedMilliseconds}ms");
+            watch.Start();
+            var firstPartResult = FirstPart.GetResult(fileLines);
+            watch.Stop();
+            Console.WriteLine($"Day 8 - First Part result: {firstPartResult}");
+            Console.WriteLine($"Elapsed time: {watch.ElapsedMilliseconds}ms");
             
             watch.Start();
             var secondPartResult = SecondPart.GetResult(fileLines);
