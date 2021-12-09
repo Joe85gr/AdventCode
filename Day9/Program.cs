10587af42f14270a34e9dc15587d457bf5c1532d
joe85gr <joe85gr>
2021-12-09 13:21:51 +0000

    feat: Day 9 part 1 added

---
+++ Day9/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+
+namespace Day9
+{
+    public static class Program
+    {
+        private const string FilePath = "Day9.txt";
+        
+        private static void Main()
+        {
+            var fileLines = File.ReadAllLines(FilePath);
+            var watch = new System.Diagnostics.Stopwatch();
+
+            watch.Start();
+            var firstPartResult = FirstPart.GetResult(fileLines);
+            watch.Stop();
+            Console.WriteLine($"Day 9 - First Part result: {firstPartResult}");
+            Console.WriteLine($"Elapsed time: {watch.ElapsedMilliseconds}ms");
+            
+            // watch.Start();
+            // var secondPartResult = SecondPart.GetResult(fileLines);
+            // watch.Stop();
+            // Console.WriteLine($"Day 8 - Second Part result: {secondPartResult}");
+            // Console.WriteLine($"Elapsed time: {watch.ElapsedMilliseconds}ms");
+        }
+    }
+}
